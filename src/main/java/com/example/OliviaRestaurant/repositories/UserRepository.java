c1fb644355a3c54acc1ed7aa722d52b18079d549
Polina24zinoveva <polina.zinoveva@list.ru>
2024-11-11 16:50:13 +0400

    Внесла правки

--- src/main/java/com/example/OliviaRestaurant/repositories/UserRepository.java
+++ src/main/java/com/example/OliviaRestaurant/repositories/UserRepository.java
@@ -4,6 +4,8 @@ package com.example.OliviaRestaurant.repositories;
 import com.example.OliviaRestaurant.models.User;
 import com.example.OliviaRestaurant.models.enums.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -12,5 +14,18 @@ public interface UserRepository extends JpaRepository<User, Long> {
     User findByPhoneNumber(String phoneNumber);
     List<User> findByName(String name);
     List<User> findBySurname(String surname);
+
+
+    List<User> findByNameContainingIgnoreCase(String name);
+    List<User> findBySurnameContainingIgnoreCase(String surname);
+    User findByPhoneNumberContaining(String phoneNumber);
+    User findByEmailContaining(String email);
+
+    // Или можно использовать аннотацию @Query для более сложных запросов
+    @Query("SELECT u FROM User u WHERE u.name LIKE %:searchQuery%")
+    List<User> findByNameLike(@Param("searchQuery") String searchQuery);
+
+    @Query("SELECT u FROM User u WHERE u.surname LIKE %:searchQuery%")
+    List<User> findBySurnameLike(@Param("searchQuery") String searchQuery);
 }
 
